27b4d7b786c14d93711bb9e5250f0f686914b395
Roman Kulyk <roman.kulyk@romankulyk.local>
2023-04-09 21:38:13 +0300

    Add React project

---
+++ app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">Watch Next App</header>
+        </div>
+    );
+}
+
+export default App;
