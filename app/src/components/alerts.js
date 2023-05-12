67727592ffc5970fdc9270aae8472516715b4d2c
Roman Kulyk <work.kulyk@gmail.com>
2023-05-12 14:32:29 +0300

    Revert "Add analytics microservice"

--- app/src/components/alerts.js
+++
@@ -1,8 +0,0 @@
-import { toast } from 'react-toastify';
-
-export const alert = (message = 'Oops, something went wrong') => {
-  return toast.error(message, {
-    position: toast.POSITION.BOTTOM_LEFT,
-    theme: 'dark'
-  });
-};
